ecfd3cbb32fa450a34f70fdcc73f2749f3333c60
Tom Roberts <Tom.Roberts@lhasalimited.org>
2018-09-20 11:06:52 +0100

    step 5 master detail components

--- src/app/heroes/heroes.component.ts
+++ src/app/heroes/heroes.component.ts
@@ -1,4 +1,7 @@
 import { Component, OnInit } from '@angular/core';
+import { Hero } from '../hero';
+import { HEROES } from '../mock-heroes';
+
 
 @Component({
   selector: 'app-heroes',
@@ -7,10 +10,20 @@ import { Component, OnInit } from '@angular/core';
 })
 export class HeroesComponent implements OnInit {
 
-  hero = 'Windstorm';
+  heroes = HEROES;
+  selectedHero: Hero;
+
+  hero: Hero = {
+    id: 1,
+    name: 'Windstorm',
+  }
   constructor() { }
 
   ngOnInit() {
+  }
+
+  onSelect(hero: Hero): void {
+    this.selectedHero = hero;
   }
 
 }
